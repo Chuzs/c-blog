c7deb6206afabed18008d6f0d4c258a2d48eee4c
Chuzs <291822616@qq.com>
2023-08-02 15:45:54 +0800

    默认语言改为中文

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,17 +3,17 @@ import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/",
-
+  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
   locales: {
     "/": {
-      lang: "en-US",
-      title: "Blog Demo",
-      description: "A blog demo for vuepress-theme-hope",
-    },
-    "/zh/": {
       lang: "zh-CN",
-      title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
+      title: "博客",
+      description: "Chuzs的博客",
+    },
+    "/en/": {
+      lang: "en-US",
+      title: "C-Blog",
+      description: "A blog of Chuzs",
     },
   },
 
